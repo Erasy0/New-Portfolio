59fed20c6b2bd265baac182b1214b2adf351f257
Erasy0 <srastuserasy@gmail.com>
2025-09-12 20:46:18 +0200

    Made changes to whole home page

---
+++ Vanta.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x7e7e7e,
+  backgroundColor: 0x0,
+  points: 9.00,
+  spacing: 17.00
+})
